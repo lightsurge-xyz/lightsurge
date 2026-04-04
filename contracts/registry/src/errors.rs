1a175437549cafddfed6903fe3dd365bc849686b
Lucky Ivanius <luckzivanius@gmail.com>
2026-04-05 02:59:23 +0700

    feat: registry contract (#1)
    * feat: init turborepo
    
    * feat: escrow x402 sdk
    
    * refactor: remove escrow
    
    * feat: service registry setup
    
    * refactor: rename service-registry to registry
    
    * feat: registry contract
    
    * chore: update project release output
    
    * fix: unclaimable rewards
    
    * chore: add dev turbo config
    
    * fix: clone and exceed u32 elapsed days

---
+++ contracts/registry/src/errors.rs
@@ -0,0 +1,19 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+#[repr(u32)]
+pub enum ContractError {
+    AlreadyRegistered = 1,
+    NotRegistered = 2,
+    InsufficientBond = 3,
+    Unauthorized = 4,
+    InvalidAmount = 5,
+    ServiceNotActive = 6,
+    CooldownNotPassed = 7,
+    NoPendingUnstake = 8,
+    PendingUnstakeExists = 9,
+    BondBelowMinimum = 10,
+    NothingToSlash = 11,
+    ZeroRewardRate = 12,
+}
